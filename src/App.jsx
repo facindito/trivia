358ce264eaaeebed03b1713db50b04149a5e4215
facindito <facinditto@gmail.com>
2023-08-10 21:55:45 -0300

    add questions

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,56 @@
+import { useEffect, useRef, useState } from 'react'
+import getQuestions from './services/getQuestions'
+
 export default function App () {
+  const [questions, setQuestions] = useState([])
+
+  const currentQuestion = questions[0] || null
+
+  useEffect(() => {
+    getQuestions()
+      .then(resp => {
+        setQuestions(resp)
+      })
+  }, [])
+
   return (
-    <div className='min-h-screen grid place-content-center '>
-      <h1 className='font-bold text-6xl'>Trivia</h1>
+    <div className='min-h-screen grid place-content-center p-4 overflow-hidden'>
+      <h1 className='font-bold text-6xl text-center mb-4'>Trivia</h1>
+      {
+        currentQuestion && (
+          <main className='max-w-4xl m-auto w-full'>
+            <h2 className='max-w-[70ch] text-center mb-4 font-semibold text-2xl'>
+              <Text value={currentQuestion.question} />
+            </h2>
+            <ul className='flex flex-col gap-4 justify-center items-center'>
+              {
+                currentQuestion.answers.map((answer, index) => (
+                  <li key={index} className='w-full'>
+                    <button className='border rounded-md w-full text-center p-4'>
+                      <Text value={answer} />
+                    </button>
+                  </li>
+                ))
+              }
+            </ul>
+          </main>
+        )
+      }
+
     </div>
+  )
+}
+
+function Text ({ value, props }) {
+  const questionText = useRef()
+
+  useEffect(() => {
+    questionText.current.innerHTML = value
+  }, [value])
+  return (
+    <div
+      {...props}
+      ref={questionText}
+    />
   )
 }
---
+++ src/services/getQuestions.js
@@ -0,0 +1,30 @@
+export default async function getQuestions ({ limit = 10 } = {}) {
+  try {
+    const response = await fetch('http://localhost:5173/trivia.json')
+    if (!response.ok) {
+      throw new Error('failed to connect API')
+    }
+    const { results } = await response.json()
+
+    const randomQuestions = results.sort(() => Math.random() - 0.5).slice(0, limit)
+
+    const formattedQuestions = randomQuestions.map((question, index) => {
+      const { correct_answer, incorrect_answers, ...rest } = question
+
+      const answers = [correct_answer, ...incorrect_answers].sort(() => Math.random() - 0.5)
+
+      const correctAnswer = answers.findIndex(answer => answer === correct_answer)
+
+      return {
+        id: index,
+        answers,
+        correctAnswer,
+        ...rest
+      }
+    })
+
+    return formattedQuestions
+  } catch (error) {
+    throw new Error(error.message)
+  }
+}
