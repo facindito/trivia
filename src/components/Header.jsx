090b430a363d78a6abb0a3b2d025a4c019a8a2f1
facindito <facinditto@gmail.com>
2023-09-14 19:35:02 -0300

    fix ui

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,31 +1,42 @@
 import { useQuestionsStore } from '../store/questions'
-import { Next, Previous } from './Icons'
+import { Next } from './Icons'
 
 export function Header () {
   const questions = useQuestionsStore(state => state.questions)
   const currentQuestion = useQuestionsStore(state => state.currentQuestion)
   const nextQuestion = useQuestionsStore(state => state.nextQuestion)
-  const previousQuestion = useQuestionsStore(state => state.previousQuestion)
+  // const previousQuestion = useQuestionsStore(state => state.previousQuestion)
 
   return (
-    <header className='flex justify-between items-center gap-4'>
-      <button
+    <header className='flex flex-col gap-4 justify-between items-end md:flex-row md:items-center'>
+      {/* <button
         onClick={previousQuestion}
         className={currentQuestion > 0 ? 'hover:text-violet-700' : ''}
         disabled={currentQuestion === 0}
       >
         <Previous />
-      </button>
-      <p className='bg-violet-700 rounded-md px-4 py-2'>
-        {currentQuestion + 1} / {questions.length}
-      </p>
+      </button> */}
+      <div className='w-full flex justify-start items-center gap-4'>
+        <strong>
+          {currentQuestion + 1} of {questions.length}
+        </strong>
+        <div className='flex-1 md:flex-none md:w-1/2 bg-gray-200 rounded-full h-2.5'>
+          <div
+            className='bg-violet-600 h-2.5 rounded-full'
+            style={{ width: `${(currentQuestion + 1) * questions.length}%` }}
+          />
+        </div>
+      </div>
+
       <button
         onClick={nextQuestion}
-        className={currentQuestion < questions.length - 1 ? 'hover:text-violet-700' : ''}
+        className='text-base px-4 py-2 border-2 rounded-md flex justify-between items-center gap-2 text-center bg-violet-700 enabled:hover:scale-105'
         disabled={currentQuestion === questions.length - 1}
       >
+        Next
         <Next />
       </button>
+
     </header>
   )
 }
